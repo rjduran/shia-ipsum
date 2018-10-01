f7dfa43516d96ffc2adf64fe4775272f28b898a4
RJ Duran <rjduranjr@gmail.com>
2018-10-01 15:55:01 -0600

    added project files

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # shia-ipsum
+
 The Motivational Ipsum
+
+![](https://github.com/rjduran/shia-ipsum/blob/master/public/img/shia.png "Just do it!")
+
+Based on [#INTRODUCTIONS (2015)](https://vimeo.com/125095515) by LaBeouf, Rönkkö & Turner in collaboration with Central Saint Martins BA Fine Art 2015 students. tl;dw: [The short version](https://www.youtube.com/watch?v=ZXsQAXx_ao0).
+
+To Run:
+
+Clone the repo and run the following. Open http://localhost:3000.
+
+```
+npm install
+node app.js
+```
+
+References
+
+* [Creating a Lorem Ipsum Generator with Node and Express](https://hackernoon.com/creating-a-lorem-ipsum-generator-with-node-and-express-9e1af0b31c86)
+* ["JUST DO IT" Motivational Speech by Shia LaBeouf](https://genius.com/Shia-labeouf-just-do-it-motivational-speech-annotated)
+* [Convert Column to Comma Separated List](https://convert.town/column-to-comma-separated-list)
+* [Free Comma Separating Tool](https://delim.co/#)
+* [Add Prefix and/or Suffix into Each Line](https://textmechanic.com/text-tools/basic-text-tools/add-prefixsuffix-into-line/)
---
+++ generator.js
@@ -0,0 +1,73 @@
+// Code that generates the random lorem ipsum text
+
+// Create a new object called loremIpsum by invoking the GenerateNewText constructor function
+const loremIpsum = new GenerateNewText();
+
+// Constructor function that creates an object with the sentences property
+function GenerateNewText() {
+  // Add property to the object
+  this.sentences =
+    [
+      "Do it.",
+      "Just do it.",
+      "Don't let your dreams be dreams.",
+      "Yesterday you said tomorrow.",
+      "So just do it.",
+      "Make your dreams come true.",
+      "Just do it.",
+      "Some people dream of success.",
+      "While you're gonna wake up and work hard at it.",
+      "Nothing is impossible.",
+      "You should get to the point.",
+      "Where anyone else would quit.",
+      "And you're not going to stop there.",
+      "No.",
+      "what are you waiting for?",
+      "Do it.",
+      "Just do it.",
+      "Yes you can.",
+      "Just do it.",
+      "If you're tired of starting over.",
+      "Stop giving up."
+   ];
+}
+
+// Method to the GenerateNewText constructor function that generates a random sentence
+GenerateNewText.prototype.getRandomSentence = function() {
+  let randomSentence = this.sentences[Math.floor(Math.random() * this.sentences.length)]
+	return randomSentence;
+}
+
+// Method to the GenerateNewText constructor function that generates a paragraph from random sentences
+GenerateNewText.prototype.getParagraph = function() {
+  let paragraph = "";
+  // Set the minimum number of words
+  let minimumCharacterLength = 250;
+  let firstSentence = true;
+  while (paragraph.length < minimumCharacterLength) {
+    if (firstSentence) {
+      paragraph = paragraph.concat(this.getRandomSentence());
+      firstSentence = false;
+    } else {
+      paragraph = paragraph.concat(" " + this.getRandomSentence());
+    }
+  }
+  return paragraph;
+}
+
+// Method to the GenerateNewText constructor function that gerates multiple paragraphs from paragraphs
+GenerateNewText.prototype.getAllParagraphs = function(numberOfParagraphs) {
+  let allParagraphs = [];
+  // Generate the number of paragraphs as specified by the user
+  while (allParagraphs.length < numberOfParagraphs) {
+    allParagraphs.push(this.getParagraph());
+  }
+  // Convert array into HTML string
+  let paragraphHTML = "";
+  allParagraphs.forEach(function (paragraph) {
+    paragraphHTML += "<p>" + paragraph + "</p>";
+  });
+  return paragraphHTML;
+}
+
+module.exports = loremIpsum;
